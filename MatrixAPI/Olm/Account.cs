81417fcd9e97f490e84d0c8a57665113cb66fe5e
Aaron <aarlin5409@hessleacademy.com>
2020-08-31 14:18:26 +0100

    added various olm methods for one time keys

--- MatrixAPI/Olm/Account.cs
+++ MatrixAPI/Olm/Account.cs
@@ -15,14 +15,6 @@ namespace MatrixAPI.Olm
         public static extern uint GetAccountSize();
 
         /// <summary>
-        /// A method to find the length of keys, given an account
-        /// </summary>
-        /// <param name="account">The account to use for key length calculation</param>
-        /// <returns>The number of bytes needed for a key</returns>
-        [DllImport("olm.dll", EntryPoint = "olm_account_identity_keys_length", ExactSpelling = true)]
-        public static extern uint GetAccountIdentityKeysLength(IntPtr account);
-
-        /// <summary>
         /// A method to initialise an account given a location in memory
         /// </summary>
         /// <param name="memory">The location in memory to initialise an account</param>
@@ -36,9 +28,17 @@ namespace MatrixAPI.Olm
         /// <param name="account">A pointer giving the memory location to create an account</param>
         /// <param name="random">A byte array containing random data</param>
         /// <param name="randomLength">The number of bits in the random byte array</param>
-        /// <returns></returns>
+        /// <returns>A response code</returns>
         [DllImport("olm.dll", EntryPoint = "olm_create_account", ExactSpelling = true)]
         public static extern uint CreateAccount(IntPtr account, byte[] random, uint randomLength);
+
+        /// <summary>
+        /// A method to find the length of keys, given an account
+        /// </summary>
+        /// <param name="account">The account to use for key length calculation</param>
+        /// <returns>The number of bytes needed for a key</returns>
+        [DllImport("olm.dll", EntryPoint = "olm_account_identity_keys_length", ExactSpelling = true)]
+        public static extern uint GetAccountIdentityKeysLength(IntPtr account);
 
         /// <summary>
         /// A method to write the identity keys of an account to a byte array
@@ -46,8 +46,40 @@ namespace MatrixAPI.Olm
         /// <param name="account">A pointer giving the memory location of an account</param>
         /// <param name="identityKeys">The byte array to write to</param>
         /// <param name="identityKeysLength">The number of bits in the byte array</param>
-        /// <returns></returns>
+        /// <returns>A response code</returns>
         [DllImport("olm.dll", EntryPoint = "olm_account_identity_keys", ExactSpelling = true)]
         public static extern uint WriteAccountIdentityKeys(IntPtr account, byte[] identityKeys, uint identityKeysLength);
+
+        /// <summary>
+        /// A method to find the length of one time keys, given an account
+        /// </summary>
+        /// <param name="account">The account to use for one time key length calculations</param>
+        /// <returns>The number of bytes needed for oen time keys</returns>
+        [DllImport("olm.dll", EntryPoint = "olm_account_one_time_keys_length", ExactSpelling = true)]
+        public static extern uint GetAccountOneTimeKeysLength(IntPtr account);
+
+        /// <summary>
+        /// A method to write the one time keys of an account to a byte array
+        /// </summary>
+        /// <param name="account">A pointer giving the memory location of an account</param>
+        /// <param name="identityKeys">The byte array to write to</param>
+        /// <param name="identityKeysLength">The number of bits in the byte array</param>
+        /// <returns>A response code</returns>
+        [DllImport("olm.dll", EntryPoint = "olm_account_one_time_keys", ExactSpelling = true)]
+        public static extern uint WriteAccountOneTimeKeys(IntPtr account, byte[] oneTimeKeys, uint oneTimeKeysLength);
+
+        /// <summary>
+        /// A method to generate a given number of one time keys
+        /// </summary>
+        /// <param name="account">The account to use</param>
+        /// <param name="numKeys">Number of keys to generate</param>
+        /// <param name="random">The random bytes to use</param>
+        /// <param name="randomLength">The number of bits in the random byte array</param>
+        /// <returns>A response  code</returns>
+        [DllImport("olm.dll", EntryPoint = "olm_account_generate_one_time_keys", ExactSpelling = true)]
+        public static extern uint GenerateAccountOneTimeKeys(IntPtr account, uint numKeys, byte[] random, uint randomLength);
+
+        [DllImport("olm.dll", EntryPoint = "olm_account_max_number_of_one_time_keys", ExactSpelling = true)]
+        public static extern uint GetAccountMaxOneTimeKeys(IntPtr account);
     }
 }
--- MatrixAPI/Olm/Helper/Keys.cs
+++ MatrixAPI/Olm/Helper/Keys.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -16,6 +17,27 @@ namespace MatrixAPI.Olm
             WriteAccountIdentityKeys(account, keys, keyLength * 8);
 
             return Convert.ToBase64String(keys);
+        }
+
+        public static JObject GetOneTimeKeys(IntPtr account)
+        {
+            //Find the length of keys in order to create buffer of correct size
+            uint keyLength = GetAccountOneTimeKeysLength(account);
+            byte[] keys = new byte[keyLength];
+
+            //Write the identity keys to the buffer
+            WriteAccountOneTimeKeys(account, keys, keyLength * 8);
+
+            string oneTimeKeys = Encoding.UTF8.GetString(keys);
+            return JObject.Parse(oneTimeKeys);
+        }
+
+        public static void GenerateOneTimeKeys(IntPtr account, uint count, Random random, uint randomLength)
+        {
+            byte[] randomBytes = new byte[randomLength];
+            random.NextBytes(randomBytes);
+
+            Olm.GenerateAccountOneTimeKeys(account, count, randomBytes, randomLength * 8);
         }
     }
 }
