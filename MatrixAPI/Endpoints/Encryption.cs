02ce1fee115c428d9dc0f080a107f3b7bac1f47d
Aaron <aarlin5409@hessleacademy.com>
2020-08-31 19:57:42 +0100

    added endpoint for uploading keys - likely to be last commit for e2e encryption until API documentation is improved

---
+++ MatrixAPI/Endpoints/Encryption.cs
@@ -0,0 +1,18 @@
+﻿using MatrixAPI.Data;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixAPI
+{
+    public partial class Matrix
+    {
+        public Response UploadIdentityKeys()
+        {
+            string logOutUrl = @$"/_matrix/client/r0/keys/upload";
+
+            return Post(logOutUrl, new JObject(), true);
+        }
+    }
+}
